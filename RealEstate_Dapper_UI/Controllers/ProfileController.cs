2845924fcb796af3788c8a4f22830e433fa5f83b
Batuhan Ertuğrul <erbatuhan255@gmail.com>
2023-08-08 16:59:46 +0300

    Add project files.

---
+++ RealEstate_Dapper_UI/Controllers/ProfileController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstate_Dapper_UI.Controllers
+{
+    public class ProfileController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
