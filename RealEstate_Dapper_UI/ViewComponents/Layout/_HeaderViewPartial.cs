d7100ea1e61ab41908dc037902208182bc04e688
Batuhan Ertuğrul <erbatuhan255@gmail.com>
2023-08-11 21:11:46 +0300

    .

---
+++ RealEstate_Dapper_UI/ViewComponents/Layout/_HeaderViewPartial.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstate_Dapper_UI.ViewComponents.Layout
+{
+    public class _HeaderViewPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
