9f9087a5ce8d22aa878814bba7646cf4274baf09
Batuhan Ertuğrul <erbatuhan255@gmail.com>
2023-08-16 15:48:35 +0300

    .

---
+++ RealEstate_Dapper_UI/ViewComponents/HomePage/_DefaultProductListExplorerCitiesPartial.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RealEstate_Dapper_UI.ViewComponents.HomePage
+{
+    public class _DefaultProductListExplorerCitiesPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
